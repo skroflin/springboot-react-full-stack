a82036c616d708875345931c40f11659d01a87c7
Sven Kroflin <99004305+skroflin@users.noreply.github.com>
2025-07-17 10:42:44 +0200

    Endpoint-ovi za ažuriranje i brisanje
    Kreirani endpoint-ovi unutar KorisnikController za ažuriranje i brisanje korisnika.

--- server-side/src/main/java/ffos/skroflin/controller/KorisnikController.java
+++ server-side/src/main/java/ffos/skroflin/controller/KorisnikController.java
@@ -5,12 +5,14 @@
 package ffos.skroflin.controller;
 
 import ffos.skroflin.model.dto.auth.JwtResponse;
+import ffos.skroflin.model.dto.korisnik.KorisnikDTO;
 import ffos.skroflin.model.dto.korisnik.KorisnikOdgovorDTO;
 import ffos.skroflin.model.dto.korisnik.KorisnikPrijavaDTO;
 import ffos.skroflin.model.dto.korisnik.KorisnikRegistracijaDTO;
 import ffos.skroflin.security.JwtTokenUtil;
 import ffos.skroflin.service.KorisnikService;
 import ffos.skroflin.service.KorisnikUserDetailsService;
+import jakarta.persistence.NoResultException;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
@@ -20,8 +22,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -76,6 +80,56 @@ public class KorisnikController {
             throw e;
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Greška prilikom dohvaćanja" + " " + e.getMessage(), e);
+        }
+    }
+    
+    @PutMapping("/put")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<KorisnikOdgovorDTO> put(
+            @RequestParam int sifra,
+            @RequestBody(required = true) KorisnikDTO dto
+    ){
+        try {
+            if (sifra <= 0) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Šifra ne smije biti manja od 0");
+            }
+            if (dto.email() == null || dto.email().isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email korisnika je obavezan!");
+            }
+            if (dto.korisnickoIme() == null || dto.korisnickoIme().isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Korisničko ime je obavezno!");
+            }
+            KorisnikOdgovorDTO azuriraniKorisnik = korisnikService.put(dto, sifra);
+            return new ResponseEntity<>(azuriraniKorisnik, HttpStatus.OK);
+        } catch (NoResultException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Greška prilikom ažuriranja" + " " + e.getMessage(),
+                    e
+            );
+        }
+    }
+    
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> delete(
+            @RequestParam int sifra
+    ){
+        try {
+            if (sifra <= 0){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Šifra ne smije biti manja od 0");
+            }
+            korisnikService.delete(sifra);
+            return new ResponseEntity<>(HttpStatus.OK);
+                } catch (NoResultException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Greška prilikom logičkog brisanja" + " " + e.getMessage(), e);
         }
     }
     
