96926184cb3a3a11d126e37f133d4885378eadd3
skroflin <99004305+skroflin@users.noreply.github.com>
2025-07-22 15:19:11 +0200

    Preuređivanje NavBar-a
    Preuređivanje komponente za navigaciju => potrebnop preurediti prikaz FontAwesome ikona unutar ruta i gumba.

--- client-side/src/components/Navbar.tsx
+++ client-side/src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaHome } from 'react-icons/fa';
+import { RiLogoutBoxRFill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
 interface NavbarProps {
@@ -16,7 +18,8 @@ const Navbar: React.FC<NavbarProps> = ({ username, isAuthenticated, onLogout }) 
         <nav className="fixed top-0 left-0 w-full bg-gray-800 p-4 text-white shadow-md z-10">
             <div className="mx-auto max-w-screen-xl flex justify-between">
                 <div className="flex items-center space-x-6">
-                    {username && <span className="font-bold text-lg mr-4">Dobrodošli, {username}!</span>}
+                    {username && <span className="font-bold text-xl mr-4">Dobrodošli, {username}!</span>}
+                    <Link to="/home" className="text-white hover:text-blue-400 transition-colors duration-200">Home<FaHome /></Link>
                     <Link to="/djelatnici" className="text-white hover:text-blue-400 transition-colors duration-200">Djelatnici</Link>
                     <Link to="/odjeli" className="text-white hover:text-blue-400 transition-colors duration-200">Odjeli</Link>
                     <Link to="/tvrtke" className="text-white hover:text-blue-400 transition-colors duration-200">Tvrtke</Link>
@@ -28,6 +31,7 @@ const Navbar: React.FC<NavbarProps> = ({ username, isAuthenticated, onLogout }) 
                         className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-2 rounded transition-colors duration-200"
                     >
                         Odjava!
+                        <RiLogoutBoxRFill />
                     </button>
                 </div>
             </div>
